c357dd5f0191bfbc62b02bcf2d932dbb5e213559
Itziman <emominwoo@gmail.com>
2023-09-21 06:56:09 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* add - Add two numbers
+*
+* @a: Firt input
+*
+* @b: Second input
+*
+* Return: Return sum of a & b
+*/
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _islower - Checks fo lowercase
+*
+* @c: char to be checked
+*
+* Return: Return 1 if c is lowercase else return 0
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
